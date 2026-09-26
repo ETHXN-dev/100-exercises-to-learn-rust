ff3e6ca3dd690fee526c7a4a944db601542c57ac
ETHXN <migethanu@gmail.com>
2026-09-26 15:12:14 +0100

    Exercise 25 (ticket_v1/outro): defined a new Order type and added an associated function and getter and setter methods

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,79 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: i32,
+    unit_price: i32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: i32, unit_price: i32) -> Self {
+        if product_name.is_empty() {
+            panic!("Product name can't be empty");
+        }
+
+        if product_name.len() > 300 {
+            panic!("Product name can't be longer than 300 bytes");
+        }
+
+        if quantity <= 0 {
+            panic!("The quantity must be strictly greater than zero");
+        }
+
+        if unit_price <= 0 {
+            panic!("The unit price must be strictly greater than zero");
+        }
+
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn total(&self) -> i32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn product_name(&self) -> String {
+        self.product_name.to_string()
+    }
+
+    pub fn quantity(&self) -> &i32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &i32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        if new_product_name.is_empty() {
+            panic!("Product name can't be empty");
+        }
+
+        if new_product_name.len() > 300 {
+            panic!("Product name can't be longer than 300 bytes");
+        }
+
+        self.product_name = new_product_name;
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: i32) {
+        if new_quantity <= 0 {
+            panic!("The quantity must be strictly greater than zero");
+        }
+
+        self.quantity = new_quantity;
+    }
+
+    pub fn set_unit_price(&mut self, new_unit_price: i32) {
+        if new_unit_price <= 0 {
+            panic!("The unit price must be strictly greater than zero");
+        }
+
+        self.unit_price = new_unit_price;
+    }
+}
